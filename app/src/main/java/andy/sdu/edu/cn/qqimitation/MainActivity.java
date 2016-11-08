bbbb72332e89b13b5580351e82b7506d097dfffd
GeekerAndyLove <GeekerAndy@outlook.com>
2016-11-08 19:01:56 +0800

    Finish three main buttons in the main layout.

---
+++ app/src/main/java/andy/sdu/edu/cn/qqimitation/MainActivity.java
@@ -0,0 +1,20 @@
+package andy.sdu.edu.cn.qqimitation;
+
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
